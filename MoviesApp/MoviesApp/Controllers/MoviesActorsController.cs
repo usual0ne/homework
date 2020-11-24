97b13260e6427fae6ea34f52e53dbfd7f7f20eff
MisterY0da <pretty_good.guy@mail.ru>
2020-11-24 17:28:35 +0300

    At the porject MoviesApp add index.cshtml to file Views/MoviesActors and add MoviesActorsController.cs to file Controllers

---
+++ MoviesApp/MoviesApp/Controllers/MoviesActorsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MoviesApp.Data;
+using MoviesApp.Models;
+using MoviesApp.ViewModels;
+
+namespace MoviesApp.Controllers
+{
+    public class MoviesActorsController : Controller
+    {
+        private readonly MoviesContext _context;
+        private readonly ILogger<HomeController> _logger;
+
+        public MoviesActorsController(MoviesContext context, ILogger<HomeController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // GET: Movies and actors
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(_context.MoviesActors.Select(ma => new MoviesActorsViewModel
+            {
+                MovieId = ma.MovieId,
+                Movie = ma.Movie,
+                ActorId = ma.ActorId,
+                Actor = ma.Actor
+
+            }).ToList());
+        }
+    }
+}
--- MoviesApp/MoviesApp/ViewModels/MoviesActorsViewModel.cs
+++ MoviesApp/MoviesApp/ViewModels/MoviesActorsViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using MoviesApp.Models;
@@ -8,9 +10,11 @@ namespace MoviesApp.ViewModels
 {
     public class MoviesActorsViewModel
     {
+        [ForeignKey("Movie")]
         public int MovieId { get; set; }
         public Movie Movie { get; set; }
 
+        [ForeignKey("Actor")]
         public int ActorId { get; set; }
         public Actor Actor { get; set; }
     }
