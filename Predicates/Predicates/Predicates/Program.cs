1f63c9a9a89f64d0d0fc7dce36d6f7e896731f3b
MisterY0da <pretty_good.guy@mail.ru>
2020-11-25 00:25:03 +0300

    add project Predicates, add Task1

---
+++ Predicates/Predicates/Predicates/Program.cs
@@ -0,0 +1,33 @@
+﻿using MiNET.Plugins;
+using System;
+using System.Collections.Generic;
+
+namespace Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+;           Console.WriteLine();
+        }
+
+        static void Task1()
+        {
+            string[] str = Console.ReadLine().Split(" ");
+            int strLastIndex = str.Length - 1;
+            Action<string> print = message => Console.WriteLine(message);
+            string[] strReverse = new string[str.Length];
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                strReverse[i] = str[strLastIndex - i];
+            }
+
+            foreach (var s in strReverse)
+            {
+                print(s);
+            }
+        }
+    }
+}
