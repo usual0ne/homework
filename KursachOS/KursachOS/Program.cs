5d33e6597d68d320c785a65094d9cb2d39b6c54f
MisterY0da <pretty_good.guy@mail.ru>
2020-12-20 00:13:26 +0300

    add project KursachOS

---
+++ KursachOS/KursachOS/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+
+namespace KursachOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputWords;            
+            using (StreamReader streamReader = new StreamReader(args[0]))
+            {
+                string input = streamReader.ReadLine();
+                inputWords = input.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+            }
+            try
+            {
+                Node<string> root = new Node<string>(inputWords[0]);
+                Tree<string> tree = new Tree<string>(root);
+
+                for(int i = 1; i < inputWords.Length; i++)
+                {
+                    Node<string> wordNode = new Node<string>(inputWords[i]);
+                    tree.AddNode(wordNode, root);
+                }
+
+                Console.WriteLine(root.Value);
+                foreach (var child in root.Children)
+                {
+                    Console.WriteLine(child.Value);
+                }
+
+                Console.WriteLine("_________________________");
+                Console.WriteLine("Delete second word");
+                Console.WriteLine("_________________________");
+
+                //second word aka first child of the root
+                tree.DeleteNode(root.Children[0], root);
+
+                Console.WriteLine(root.Value);
+                foreach (var child in root.Children)
+                {
+                    Console.WriteLine(child.Value);
+                }
+
+                Console.WriteLine("_________________________");
+                Console.WriteLine("Add one more word");
+                Console.WriteLine("_________________________");
+
+                Node<string> flashcard = new Node<string>("flashcard");
+                tree.AddNode(flashcard, root);
+
+                Console.WriteLine(root.Value);
+                foreach (var child in root.Children)
+                {
+                    Console.WriteLine(child.Value);
+                }
+
+                File.WriteAllText(args[0], "");
+
+                using (StreamWriter streamWriter = new StreamWriter(args[0], true, System.Text.Encoding.Default))
+                {
+                    streamWriter.WriteLine(root.Value);
+                    foreach (var child in root.Children)
+                    {
+                        streamWriter.WriteLine(child.Value);
+                    }
+                }
+
+
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ KursachOS/KursachOS/Tree.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KursachOS
+{
+    public class Tree<T> where T : IComparable
+    {
+        public Node<T> Root { get; set; }
+
+        public Tree(Node<T> root)
+        {
+            this.Root = root;
+        }
+
+
+        //Prints path from the root to chosen node
+        public void PrintPath(List<Node<T>> path, string delimiter)
+        {
+            for(int i = path.Count - 1; i > 0; i--)
+            {
+                Console.Write(path[i].Value + delimiter);
+            }
+            Console.Write(path[0].Value);
+        }
+
+        //Finds path from the root to chosen node
+       public List<Node<T>> FindPath(Node<T> searchNode, List<Node<T>> orderBFSList)
+       {
+            List<Node<T>> path = new List<Node<T>>();
+
+            if (orderBFSList.Contains(searchNode))
+            {
+                var current = searchNode;
+                path.Add(current);
+
+                //search for connected to searchNode vertices beginning from the end of orderBFSList
+                int index = orderBFSList.IndexOf(searchNode);
+                for (int i = index; i >= 0; i--)
+                {
+                    if (orderBFSList[i].Value.CompareTo(current.Parent.Value) == 0)
+                    {
+                        path.Add(orderBFSList[i]);
+                        current = orderBFSList[i];
+                    }
+                }
+            }
+
+            return path;
+        }
+
+        public List<Node<T>> orderBFS(Node<T> root)
+        {
+            var queue = new Queue<Node<T>>();
+            var result = new List<Node<T>>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                Node<T> node = queue.Dequeue();
+                result.Add(node);
+                foreach (var n in node.Children)
+                {
+                    queue.Enqueue(n);
+                }
+            }
+
+            return result;
+        }
+
+        public void AddNode(Node<T> insertionNode, Node<T> connectionNode)
+        {
+            connectionNode.Children.Add(insertionNode);
+            insertionNode.Parent = connectionNode;
+        }
+
+        public void DeleteNode(Node<T> deletionNode, Node<T> connectionNode)
+        {
+            deletionNode.Parent = null;
+            connectionNode.Children.Remove(deletionNode);
+        }      
+    }
+}
+
